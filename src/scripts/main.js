b5252afe039759386acf35e8c456b143ad184f99
CJ Ponti <cjponti@gmail.com>
2015-01-11 10:29:22 +0000

    Refactor main.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,12 +5,29 @@ window.jQuery = require('jquery');
 
 // Require Angular and dependencies
 require('angular');
+require('angular-animate');
 require('angular-ui-router');
 
+
+// Require templates
 require('./templates');
 
+// Require directives
+require('./directives');
+
+// Declare 'devnotes' module
 angular.module('devnotes', [
-  'devnotes.notes'
+  'ngAnimate',
+  'ui.router',
+  'devnotes.notes',
+  'devnotes.directives'
 ]);
 
+// Configure application
+angular.module('devnotes').config(require('./config'));
+
+angular.module('devnotes')
+  .controller('HomeController', require('./home-controller'));
+
+// Bootstrap 'devnots' module to `document`
 angular.bootstrap(document, ['devnotes']);
