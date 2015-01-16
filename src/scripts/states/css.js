6ebf2ad994453ff6b1713d38d1468c21742cdd03
CJ Ponti <cjponti@gmail.com>
2015-01-16 12:25:35 +0000

    Add new css states

--- src/scripts/states/css.js
+++ src/scripts/states/css.js
@@ -6,6 +6,10 @@ module.exports = function(stateProvider) {
       url: '/css',
       templateUrl: 'pages/css.html'
     })
+    .state('css.animation', {
+      url: '/animation',
+      templateUrl: 'notes/css/animation.html',
+    })
     .state('css.box-model', {
       url: '/box-model',
       templateUrl: 'notes/css/box-model.html'
@@ -14,9 +18,29 @@ module.exports = function(stateProvider) {
       url: '/smacss',
       templateUrl: 'notes/css/smacss.html',
     })
+    .state('css.naming', {
+      url: '/naming',
+      templateUrl: 'notes/css/naming.html',
+    })
+    .state('css.transform', {
+      url: '/transform',
+      templateUrl: 'notes/css/transform.html',
+    })
+    .state('css.transition', {
+      url: '/transition',
+      templateUrl: 'notes/css/transition.html',
+    })
     .state('css.position', {
       url: '/position',
       templateUrl: 'notes/css/position.html',
+    })
+    .state('css.media-queries', {
+      url: '/media-queries',
+      templateUrl: 'notes/css/media-queries.html',
+    })
+    .state('css.methodology', {
+      url: '/methodology',
+      templateUrl: 'notes/css/methodology.html',
     })
     .state('css.bem', {
       url: '/bem',
