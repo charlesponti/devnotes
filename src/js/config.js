f794e397e8a491e963e4005402b0ae6c790581d2
CJ Ponti <cjponti@gmail.com>
2015-01-17 23:10:01 +0000

    Update templateUrl for /tools url

--- src/js/config.js
+++ src/js/config.js
@@ -29,7 +29,7 @@ module.exports = function($stateProvider, $urlRouterProvider) {
     })
     .state('tools', {
       url: '/tools',
-      templateUrl: 'tools.html'
+      templateUrl: 'notes/tools.html'
     });
 
 };
